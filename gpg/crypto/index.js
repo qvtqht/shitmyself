0573bd5cfabd55ecc515e6e54ef1a36fb091fff7
ilya <@>
2019-04-19 00:17:45 -0400

    remove nested openpgp distro that is not needed

--- gpg/crypto/index.js
+++
@@ -1,9 +0,0 @@
-describe('Crypto', function () {
-  require('./cipher');
-  require('./hash');
-  require('./random.js');
-  require('./crypto.js');
-  require('./elliptic.js');
-  require('./pkcs5.js');
-  require('./aes_kw.js');
-});
