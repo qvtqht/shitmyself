85b407129a1a989b3fce8a7bff976d27a5d89644
ilya <@>
2019-04-30 01:51:41 -0400

    update upgrade.pl

--- upgrade.pl
+++ upgrade.pl
@@ -1,18 +1,22 @@
 #!/usr/bin/perl
 
-die();
-
 use strict;
 use utf8;
 use warnings;
 
 my $time = time();
 
-my $stashCommand = "git stash >> stash_$time.txt";
-system($stashCommand);
+if (-e 'cron.lock') {
+	die('cron.lock exists');
+}
+
+system(`echo $time > cron.lock`);
+
+#my $stashCommand = "git stash >> stash_$time.txt";
+#system($stashCommand);
 
 my $pullCommand = "git pull >> pull_$time.txt";
 system($pullCommand);
 
-my $unstashCommand = "git stash pop >> unstash_$time.txt";
-system($unstashCommand);
+#my $unstashCommand = "git stash pop >> unstash_$time.txt";
+#system($unstashCommand);
