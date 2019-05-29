04487d396bb8accc44eb194e8e1d839ca3bb9cc9
ilya <@>
2019-05-28 23:10:41 -0400

    add call to WriteIndexedConfig() to gitflow.pl

--- gitflow.pl
+++ gitflow.pl
@@ -177,6 +177,8 @@ foreach my $file (@gitChangesArray) {
 	}
 }
 
+WriteIndexedConfig();
+
 # get a list of pages that have been touched since the last git_flow
 # this is from the page_touch table
 my $touchedPages = DBGetTouchedPages($lastFlow);
