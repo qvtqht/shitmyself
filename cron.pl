ee420f07fad204707b5b5cb0010a97d0f860022a
ilya <@>
2019-07-20 14:10:29 -0400

    replace time() with GetTime()

--- cron.pl
+++ cron.pl
@@ -5,7 +5,7 @@ require './utils.pl';
 require './access.pl';
 
 my $lockTime = GetFile('cron.lock');
-my $currentTime = time();
+my $currentTime = GetTime();
 
 if ($lockTime) {
 	if ($currentTime - 1800 < $lockTime) {
@@ -27,11 +27,11 @@ if (GetConfig('admin/git_cron_pull') == 1) {
 my $accessLogPath = GetConfig('admin/access_log_path');
 WriteLog("\$accessLogPath = $accessLogPath");
 
-my $startTime = time();
+my $startTime = GetTime();
 my $interval = GetConfig('admin/cron_continue');
 my $touch = 0;
 
-while (!$touch || time() < $startTime + $interval) {
+while (!$touch || GetTime() < $startTime + $interval) {
 	$touch = 1;
 
 	if (!GetFile('cron.lock') || GetFile('cron.lock') ne $lockTime) {
