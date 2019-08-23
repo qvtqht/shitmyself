cdfa3f7733c06eaf04a2444264844db219720d14
ilya <@>
2019-08-22 21:50:48 -0400

    add loop.pl

---
+++ loop.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+# runs gitflow.pl in a loop
+# pauses for 2 seconds in between
+
+use strict;
+use warnings;
+
+system('rm cron.lock');
+
+while (!system('perl ./gitflow.pl')) {
+	print '
+  _                          _  
+ | | ___   ___  _ __   _ __ | | 
+ | |/ _ \ / _ \| \'_ \ | \'_ \| | 
+ | | (_) | (_) | |_) || |_) | | 
+ |_|\___/ \___/| .__(_) .__/|_| 
+               |_|    |_|       
+';
+
+	sleep 2;
+}
+
+1;
