a404df027f15100c86ec4303f50cf72c8108b5f6
ilya <@>
2019-10-12 17:09:02 -0400

    refactor gitflow.pl

--- gitflow.pl
+++ gitflow.pl
@@ -25,313 +25,320 @@ chomp $SCRIPTDIR;
 print GetTime2() . " Begin requires\n";
 
 require './utils.pl';
-require './sqlite.pl';
-require './index.pl';
-require './access.pl';
-require './pages.pl';
-
-print GetTime() . " End requires\n";
-
-WriteLog('gitflow.pl begin');
-
-my %counter;
-$counter{'access_log'} = 0;
-$counter{'indexed_file'} = 0;
 
 my $lockTime = GetFile('cron.lock');
 my $currentTime = GetTime2();
 
+my $locked = 0;
+
 if ($lockTime) {
 	if ($currentTime - 1800 < $lockTime) {
 		WriteLog('Quitting due to lock file');
-		die('Quitting due to lock file');
+		$locked = 1;
 	} else {
 		WriteLog('Lock file exists, but old. Continuing.');
 	}
 }
-PutFile('cron.lock', $currentTime);
-$lockTime = $currentTime;
 
-# store the last time we did this from config/admin/gitflow/last_run
-my $lastFlow = GetConfig('admin/gitflow/last_run');
+if (!$locked) {
+	require './sqlite.pl';
+	require './index.pl';
+	require './access.pl';
+	require './pages.pl';
+	
+	print GetTime() . " End requires\n";
+	
+	WriteLog('gitflow.pl begin');
+	
+	my %counter;
+	$counter{'access_log'} = 0;
+	$counter{'indexed_file'} = 0;
 
-if ($lastFlow) {
-	WriteLog('$lastFlow = ' . $lastFlow);
-} else {
-	WriteLog('$lastFlow undefined');
-	$lastFlow = 0;
-}
 
-# get the path of access log, usually log/access.log
-my $accessLogPath = GetConfig('admin/access_log_path');
-WriteLog("\$accessLogPath = $accessLogPath");
-
-# this will store the new item count we get from access.log
-my $newItemCount;
-
-# time limit
-my $timeLimit = GetConfig('admin/gitflow/time_limit');
-my $startTime = GetTime2();
-#todo validation
-
-my $accessLogPathsConfig = GetConfig('admin/access_log_path_list');
-my @accessLogPaths;
-if ($accessLogPathsConfig) {
-	@accessLogPaths = split("\n", $accessLogPathsConfig);
-} else {
-	push @accessLogPaths, GetConfig('admin/access_log_path');
-}
-
-#todo re-test this
-foreach my $accessLogPath(@accessLogPaths) {
-	# Check to see if access log exists
-	if (-e $accessLogPath) {
-		#Process the access log (access.pl)
-		$newItemCount += ProcessAccessLog($accessLogPath, 0);
-
-		WriteLog("Processed $accessLogPath; \$newItemCount = $newItemCount");
-
-		$counter{'access_log'} += $newItemCount;
+	PutFile('cron.lock', $currentTime);
+	$lockTime = $currentTime;
+	
+	# store the last time we did this from config/admin/gitflow/last_run
+	my $lastFlow = GetConfig('admin/gitflow/last_run');
+	
+	if ($lastFlow) {
+		WriteLog('$lastFlow = ' . $lastFlow);
 	} else {
-		WriteLog("WARNING: Could not find $accessLogPath");
+		WriteLog('$lastFlow undefined');
+		$lastFlow = 0;
 	}
-}
-
-# check if html/txt/ has its own git repository
-# init a new repo in html/txt/ if html/txt/.git/ is missing
-
-if (!-e 'html') {
-	system('mkdir html');
-}
-
-if (!-d 'html') {
-	WriteLog('Problem!!! html is not a directory!');
-}
-
-if (!-e 'html/txt') {
-	system('mkdir html/txt');
-}
-
-if (!-d 'html/txt') {
-	WriteLog('Problem!!! html/txt is not a directory!');
-}
-
-if (!-e 'html/txt/.git') {
-	my $pwd = `pwd`;
-
-#	WriteLog("cd html/txt; git init; cd $pwd");
-	WriteLog("cd html/txt; git init; git add *; git commit -m first commit; cd $pwd");
-
-#	my $gitOutput = `cd html/txt; git init; git add *; git commit -m first commit; cd $pwd`;
-
-#	WriteLog($gitOutput);
-}
-
-# See if gitflow/file_limit setting exists
-# This limits the number of files to process per launch of gitflow.pl
-my $filesLimit = GetConfig('admin/gitflow/file_limit');
-if (!$filesLimit) {
-	WriteLog("WARNING: config/admin/gitflow/file_limit missing!");
-	$filesLimit = 100;
-}
-
-# Use git to find files that have changed in txt/ directory
-WriteLog("\$gitChanges = cd html/txt; git add . ; git status --porcelain | grep \"^A\" | head -n $filesLimit | cut -c 4-; cd ../..");
-my $gitChanges = `cd html/txt; git add . ; git status --porcelain | grep "^A" | head -n $filesLimit | cut -c 4-; cd ../..`;
-
-WriteLog('$gitChanges = ' . $gitChanges);
-
-# Get an array of changed files that git returned
-my @gitChangesArray = split("\n", $gitChanges);
-
-# Log number of changes
-WriteLog('scalar(@gitChangesArray) = ' . scalar(@gitChangesArray));
-                                                                                                                                
-# Keep track of how many files we've processed
-my $filesProcessed = 0;
-
-# Go through all the changed files
-foreach my $file (@gitChangesArray) {
-	# Add to counter
-	$filesProcessed++;
-	if ($filesProcessed > $filesLimit) {
-		WriteLog("Will not finish processing files, as limit of $filesLimit has been reached.");
-		last;
+	
+	# get the path of access log, usually log/access.log
+	my $accessLogPath = GetConfig('admin/access_log_path');
+	WriteLog("\$accessLogPath = $accessLogPath");
+	
+	# this will store the new item count we get from access.log
+	my $newItemCount;
+	
+	# time limit
+	my $timeLimit = GetConfig('admin/gitflow/time_limit');
+	my $startTime = GetTime2();
+	#todo validation
+	
+	my $accessLogPathsConfig = GetConfig('admin/access_log_path_list');
+	my @accessLogPaths;
+	if ($accessLogPathsConfig) {
+		@accessLogPaths = split("\n", $accessLogPathsConfig);
+	} else {
+		push @accessLogPaths, GetConfig('admin/access_log_path');
 	}
-
-	if ((GetTime2() - $startTime) > $timeLimit) {
-		WriteLog("Time limit reached, exiting loop");
-		last;
-	}
-
-	# Trim the file path
-	$file = trim($file);
-
-	# Add the txt/ path prefix
-	my $fileFullPath = 'html/txt/' . $file;
-
-	# Log it
-	WriteLog('$file = ' . $file . " ($fileFullPath)");
-		
-	#todo add rss.txt addition
-
-	# If the file exists, and is not a directory, process it
-	if (-e $fileFullPath && !-d $fileFullPath) {
-		my $addedTime = GetTime2();
-
-		# get file's hash from git
-		my $fileHash = GetFileHash($fileFullPath);
-
-		# if deletion of this file has been requested, skip
-		if (-e 'log/deleted.log' && GetFile('log/deleted.log') =~ $fileHash) {
-			unlink($fileFullPath);
-			WriteLog("gitflow.pl: $fileHash exists in deleted.log, skipping");
-			next;
+	
+	#todo re-test this
+	foreach my $accessLogPath(@accessLogPaths) {
+		# Check to see if access log exists
+		if (-e $accessLogPath) {
+			#Process the access log (access.pl)
+			$newItemCount += ProcessAccessLog($accessLogPath, 0);
+	
+			WriteLog("Processed $accessLogPath; \$newItemCount = $newItemCount");
+	
+			$counter{'access_log'} += $newItemCount;
+		} else {
+			WriteLog("WARNING: Could not find $accessLogPath");
 		}
-
-		my $fileHashPath;
-		# may need to check if file has been renamed
-		if (GetConfig('admin/organize_files')) {
-			$fileHashPath = GetFileHashPath($fileFullPath);
+	}
+	
+	# check if html/txt/ has its own git repository
+	# init a new repo in html/txt/ if html/txt/.git/ is missing
+	
+	if (!-e 'html') {
+		system('mkdir html');
+	}
+	
+	if (!-d 'html') {
+		WriteLog('Problem!!! html is not a directory!');
+	}
+	
+	if (!-e 'html/txt') {
+		system('mkdir html/txt');
+	}
+	
+	if (!-d 'html/txt') {
+		WriteLog('Problem!!! html/txt is not a directory!');
+	}
+	
+	if (!-e 'html/txt/.git') {
+		my $pwd = `pwd`;
+	
+	#	WriteLog("cd html/txt; git init; cd $pwd");
+		WriteLog("cd html/txt; git init; git add *; git commit -m first commit; cd $pwd");
+	
+	#	my $gitOutput = `cd html/txt; git init; git add *; git commit -m first commit; cd $pwd`;
+	
+	#	WriteLog($gitOutput);
+	}
+	
+	# See if gitflow/file_limit setting exists
+	# This limits the number of files to process per launch of gitflow.pl
+	my $filesLimit = GetConfig('admin/gitflow/file_limit');
+	if (!$filesLimit) {
+		WriteLog("WARNING: config/admin/gitflow/file_limit missing!");
+		$filesLimit = 100;
+	}
+	
+	# Use git to find files that have changed in txt/ directory
+	WriteLog("\$gitChanges = cd html/txt; git add . ; git status --porcelain | grep \"^A\" | head -n $filesLimit | cut -c 4-; cd ../..");
+	my $gitChanges = `cd html/txt; git add . ; git status --porcelain | grep "^A" | head -n $filesLimit | cut -c 4-; cd ../..`;
+	
+	WriteLog('$gitChanges = ' . $gitChanges);
+	
+	# Get an array of changed files that git returned
+	my @gitChangesArray = split("\n", $gitChanges);
+	
+	# Log number of changes
+	WriteLog('scalar(@gitChangesArray) = ' . scalar(@gitChangesArray));
+																																	
+	# Keep track of how many files we've processed
+	my $filesProcessed = 0;
+	
+	# Go through all the changed files
+	foreach my $file (@gitChangesArray) {
+		# Add to counter
+		$filesProcessed++;
+		if ($filesProcessed > $filesLimit) {
+			WriteLog("Will not finish processing files, as limit of $filesLimit has been reached.");
+			last;
 		}
-
-		# index file, flush immediately (why? #todo)
-		IndexTextFile($fileFullPath);
-		IndexTextFile('flush');
-
-		# check if file has been renamed
-		if (GetConfig('admin/organize_files') && $fileHashPath) {
-			if (!-e $fileFullPath) {
-				if (-e $fileHashPath) {
-					$fileFullPath = $fileHashPath;
+	
+		if ((GetTime2() - $startTime) > $timeLimit) {
+			WriteLog("Time limit reached, exiting loop");
+			last;
+		}
+	
+		# Trim the file path
+		$file = trim($file);
+	
+		# Add the txt/ path prefix
+		my $fileFullPath = 'html/txt/' . $file;
+	
+		# Log it
+		WriteLog('$file = ' . $file . " ($fileFullPath)");
+			
+		#todo add rss.txt addition
+	
+		# If the file exists, and is not a directory, process it
+		if (-e $fileFullPath && !-d $fileFullPath) {
+			my $addedTime = GetTime2();
+	
+			# get file's hash from git
+			my $fileHash = GetFileHash($fileFullPath);
+	
+			# if deletion of this file has been requested, skip
+			if (-e 'log/deleted.log' && GetFile('log/deleted.log') =~ $fileHash) {
+				unlink($fileFullPath);
+				WriteLog("gitflow.pl: $fileHash exists in deleted.log, skipping");
+				next;
+			}
+	
+			my $fileHashPath;
+			# may need to check if file has been renamed
+			if (GetConfig('admin/organize_files')) {
+				$fileHashPath = GetFileHashPath($fileFullPath);
+			}
+	
+			# index file, flush immediately (why? #todo)
+			IndexTextFile($fileFullPath);
+			IndexTextFile('flush');
+	
+			# check if file has been renamed
+			if (GetConfig('admin/organize_files') && $fileHashPath) {
+				if (!-e $fileFullPath) {
+					if (-e $fileHashPath) {
+						$fileFullPath = $fileHashPath;
+					}
 				}
 			}
-		}
-
-		# add a time_added record
-		DBAddAddedTimeRecord($fileHash, $addedTime);
-
-		# remember pwd (current working directory);
-		my $pwd = `pwd`;
-
-		# run commands to
-		#	  add changed file to git repo
-		#    commit the change with message 'hi' #todo
-		#    cd back to pwd
-		
-		my $fileGitPath = substr($fileFullPath, 9);
-		
-		WriteLog("cd html/txt; git add \"$fileGitPath\"; git commit -m hi \"$fileGitPath\"; cd $pwd");
-		my $gitCommit = `cd html/txt; git add "$fileGitPath"; git commit -m hi "$fileGitPath"; cd $pwd`;
-		#todo there's a minor bug here related to organize_files
-
-		# write git's output to log
-		WriteLog($gitCommit);
-
-#		# below is for debugging purposes
-#
-#		my %queryParams;
-#		$queryParams{'where_clause'} = "WHERE file_hash = '$fileHash'";
-#
-#		my @files = DBGetItemList(\%queryParams);
-#
-#		WriteLog ("Count of new items for $fileHash : " . scalar(@files));
-	} else {
-		# this should not happen
-		WriteLog("Error! $fileFullPath doesn't exist!");
-	}
-}
-
-WriteIndexedConfig();
-
-RemoveEmptyDirectories('./html/'); #includes txt/
-#RemoveEmptyDirectories('./txt/');
-
-# if new items were added, re-make all the summary pages (top authors, new threads, etc)
-if ($filesProcessed > 0) {
-	UpdateUpdateTime();
-	MakeSummaryPages();
-#	WriteIndexPages();
-}
-
-# get a list of pages that have been touched since the last git_flow
-# this is from the page_touch table
-my $touchedPages = DBGetTouchedPages($lastFlow);
-
-# de-reference array of touched pages
-my @touchedPagesArray = @$touchedPages;
-
-# write number of touched pages to log
-WriteLog('scalar(@touchedPagesArray) = ' . scalar(@touchedPagesArray));
-
-my $pagesLimit = GetConfig('admin/gitflow/page_limit');
-if (!$pagesLimit) {
-	WriteLog("WARNING: config/admin/gitflow/page_limit missing!");
-	$pagesLimit = 100;
-}
-my $pagesProcessed = 0;
-
-# this part will refresh any pages that have been "touched"
-# in this case, 'touch' means when an item that affects the page
-# is updated or added
-foreach my $page (@touchedPagesArray) {
-#	$pagesProcessed++;
-#	if ($pagesProcessed > $pagesLimit) {
-#		WriteLog("Will not finish processing pages, as limit of $pagesLimit has been reached");
-#		last;
-#	}
-#	if ((GetTime2() - $startTime) > $timeLimit) {
-#		WriteLog("Time limit reached, exiting loop");
-#		last;
-#	}
-
-	# dereference @pageArray
-	my @pageArray = @$page;
-
-	# get the 3 items in it
-	my $pageType = shift @pageArray;
-	my $pageParam = shift @pageArray;
-	my $touchTime = shift @pageArray;
-
-	# output to log
-	WriteLog("\$pageType = $pageType");
-	WriteLog("\$pageParam = $pageParam");
-	WriteLog("\$touchTime = $touchTime");
-
-	MakePage($pageType, $pageParam);
 	
-	DBDeletePageTouch($pageType, $pageParam);
+			# add a time_added record
+			DBAddAddedTimeRecord($fileHash, $addedTime);
+	
+			# remember pwd (current working directory);
+			my $pwd = `pwd`;
+	
+			# run commands to
+			#	  add changed file to git repo
+			#    commit the change with message 'hi' #todo
+			#    cd back to pwd
+			
+			my $fileGitPath = substr($fileFullPath, 9);
+			
+			WriteLog("cd html/txt; git add \"$fileGitPath\"; git commit -m hi \"$fileGitPath\"; cd $pwd");
+			my $gitCommit = `cd html/txt; git add "$fileGitPath"; git commit -m hi "$fileGitPath"; cd $pwd`;
+			#todo there's a minor bug here related to organize_files
+	
+			# write git's output to log
+			WriteLog($gitCommit);
+	
+	#		# below is for debugging purposes
+	#
+	#		my %queryParams;
+	#		$queryParams{'where_clause'} = "WHERE file_hash = '$fileHash'";
+	#
+	#		my @files = DBGetItemList(\%queryParams);
+	#
+	#		WriteLog ("Count of new items for $fileHash : " . scalar(@files));
+		} else {
+			# this should not happen
+			WriteLog("Error! $fileFullPath doesn't exist!");
+		}
+	}
+	
+	WriteIndexedConfig();
+	
+	RemoveEmptyDirectories('./html/'); #includes txt/
+	#RemoveEmptyDirectories('./txt/');
+	
+	# if new items were added, re-make all the summary pages (top authors, new threads, etc)
+	if ($filesProcessed > 0) {
+		UpdateUpdateTime();
+		MakeSummaryPages();
+	#	WriteIndexPages();
+	}
+	
+	# get a list of pages that have been touched since the last git_flow
+	# this is from the page_touch table
+	my $touchedPages = DBGetTouchedPages($lastFlow);
+	
+	# de-reference array of touched pages
+	my @touchedPagesArray = @$touchedPages;
+	
+	# write number of touched pages to log
+	WriteLog('scalar(@touchedPagesArray) = ' . scalar(@touchedPagesArray));
+	
+	my $pagesLimit = GetConfig('admin/gitflow/page_limit');
+	if (!$pagesLimit) {
+		WriteLog("WARNING: config/admin/gitflow/page_limit missing!");
+		$pagesLimit = 100;
+	}
+	my $pagesProcessed = 0;
+	
+	# this part will refresh any pages that have been "touched"
+	# in this case, 'touch' means when an item that affects the page
+	# is updated or added
+	foreach my $page (@touchedPagesArray) {
+	#	$pagesProcessed++;
+	#	if ($pagesProcessed > $pagesLimit) {
+	#		WriteLog("Will not finish processing pages, as limit of $pagesLimit has been reached");
+	#		last;
+	#	}
+	#	if ((GetTime2() - $startTime) > $timeLimit) {
+	#		WriteLog("Time limit reached, exiting loop");
+	#		last;
+	#	}
+	
+		# dereference @pageArray
+		my @pageArray = @$page;
+	
+		# get the 3 items in it
+		my $pageType = shift @pageArray;
+		my $pageParam = shift @pageArray;
+		my $touchTime = shift @pageArray;
+	
+		# output to log
+		WriteLog("\$pageType = $pageType");
+		WriteLog("\$pageParam = $pageParam");
+		WriteLog("\$touchTime = $touchTime");
+	
+		MakePage($pageType, $pageParam);
+		
+		DBDeletePageTouch($pageType, $pageParam);
+	}
+	
+	# if anything has changed, redo the abyss index pages
+	#if ($newItemCount) {
+		#WriteIndexPages();
+	#}
+	
+	## rebuild abyss pages no more than once an hour (default/admin/abyss_rebuild_interval)
+	#my $lastAbyssRebuild = GetConfig('last_abyss');
+	#my $abyssRebuildInterval = GetConfig('admin/abyss_rebuild_interval');
+	#my $curTime = GetTime2();
+	#
+	#WriteLog("Abyss was last rebuilt at $lastAbyssRebuild, and now it is $curTime");
+	#if (!($lastAbyssRebuild =~ /^[0-9]+/)) {
+	#	$lastAbyssRebuild = 0;
+	#}
+	#if ((!$lastAbyssRebuild) || (($lastAbyssRebuild + $abyssRebuildInterval) < $curTime)) {
+	#	WriteLog("Rebuilding Abyss, because " . ($lastAbyssRebuild + 86400) . " < " . $curTime);
+	#	WriteIndexPages();
+	#	PutConfig('last_abyss', $curTime);
+	#}
+	
+	# save current time in config/admin/gitflow/last
+	my $newLastFlow = GetTime2();
+	WriteLog($newLastFlow);
+	PutConfig('admin/gitflow/last', $newLastFlow);
+	
+	unlink('cron.lock');
+	
+	WriteLog("======gitflow.pl DONE! ======");
+	WriteLog("Items/files processed: $filesProcessed");
+	print("Items/files processed: $filesProcessed\n");
 }
-
-# if anything has changed, redo the abyss index pages
-#if ($newItemCount) {
-	#WriteIndexPages();
-#}
-
-## rebuild abyss pages no more than once an hour (default/admin/abyss_rebuild_interval)
-#my $lastAbyssRebuild = GetConfig('last_abyss');
-#my $abyssRebuildInterval = GetConfig('admin/abyss_rebuild_interval');
-#my $curTime = GetTime2();
-#
-#WriteLog("Abyss was last rebuilt at $lastAbyssRebuild, and now it is $curTime");
-#if (!($lastAbyssRebuild =~ /^[0-9]+/)) {
-#	$lastAbyssRebuild = 0;
-#}
-#if ((!$lastAbyssRebuild) || (($lastAbyssRebuild + $abyssRebuildInterval) < $curTime)) {
-#	WriteLog("Rebuilding Abyss, because " . ($lastAbyssRebuild + 86400) . " < " . $curTime);
-#	WriteIndexPages();
-#	PutConfig('last_abyss', $curTime);
-#}
-
-# save current time in config/admin/gitflow/last
-my $newLastFlow = GetTime2();
-WriteLog($newLastFlow);
-PutConfig('admin/gitflow/last', $newLastFlow);
-
-unlink('cron.lock');
-
-WriteLog("======gitflow.pl DONE! ======");
-WriteLog("Items/files processed: $filesProcessed");
-print("Items/files processed: $filesProcessed\n");
 
 1;
