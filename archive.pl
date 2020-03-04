971c430d631f34a4a1484ae76cdb4a98349ab440
ily <@>
2020-03-04 08:42:58 -0500

    add archive.pl

---
+++ archive.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+my $date = `date +%s`;
+chomp $date;
+
+if (!-e archive) {
+	mkdir 'archive';
+}
+
+system("mkdir archive/$date");
+system("mv html archive/$date");
+system("cp -r config archive/$date");
+system("mv log archive/$date");
+system("rm cron.lock");
+
+system("mkdir html");
+system("mkdir html/txt");
+system("echo \"archived at $date\" > html/txt/archived_$date\.txt");
