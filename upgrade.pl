1e826691d8651beaefd6d23616bff4e3e54d589d
ily <@>
2020-04-13 19:17:04 -0400

    minor development of upgrade.pl

--- upgrade.pl
+++ upgrade.pl
@@ -1,22 +1,30 @@
 #!/usr/bin/perl
 
+die;
+
 use strict;
 use utf8;
 use warnings;
 
 my $time = time();
-
-if (-e 'cron.lock') {
-	die('cron.lock exists');
-}
-
-system(`echo $time > cron.lock`);
-
+#
+# if (-e 'cron.lock') {
+# 	die('cron.lock exists');
+# }
+#
+# system(`echo $time > cron.lock`);
+#
 #my $stashCommand = "git stash >> stash_$time.txt";
 #system($stashCommand);
 
+print (`cd /home/ily/hike`);
+
 my $pullCommand = "git pull >> pull_$time.txt";
-system($pullCommand);
+print($pullCommand);
+print(`$pullCommand`);
+
+my $buildCommand = "cd /home/ily/hike ; ./clean.pl ; ./build.pl";
+print(`$buildCommand`);
 
 #my $unstashCommand = "git stash pop >> unstash_$time.txt";
 #system($unstashCommand);
