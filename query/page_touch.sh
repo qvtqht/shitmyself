b7bd349d9bd22462cfe68959bbad486217d0c3f0
ily <@>
2020-07-07 23:43:56 -0400

    add comments to page_touch.sh

--- query/page_touch.sh
+++ query/page_touch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# displays page_touch table, sorted by relevance
+# this table is queue for generating pages
+
+# this script assumes existence of one cache/*/index.sqlite
+
 sqlite3 -echo -cmd ".headers off" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 "SELECT count(*) FROM page_touch WHERE priority > 0; SELECT * FROM page_touch ORDER BY priority DESC, touch_time DESC LIMIT 25"
 
