61db3a3437a3454546a69f669ecf3fe015602293
toshiba <@>
2020-08-24 15:59:42 -0400

    reduce interval between access.log checks

--- update.pl
+++ update.pl
@@ -325,8 +325,8 @@ if (!defined($arg1) || $arg1 eq '--all') {
 			$timeLimit = 10;
 		}
 
-		if ((time()-(GetConfig('admin/access_log_last_seen')||0)) > 3600) { #todo make this more configurable
-			#do not process access.log more than once per hour
+		if ((time()-(GetConfig('admin/access_log_last_seen')||0)) > 100) { #todo make this more configurable
+			#do not process access.log more than once per 100 seconds
 			PutConfig('admin/access_log_last_seen', time());
 
 			WriteMessage('Reading access.log...');
