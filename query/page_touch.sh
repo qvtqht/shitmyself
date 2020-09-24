f5b30f74985c617587cb80b36874b8ae6035e0bf
toshiba <@>
2020-09-24 05:35:13 -0400

    Headers on for page queue query

--- query/page_touch.sh
+++ query/page_touch.sh
@@ -5,5 +5,5 @@
 
 # this script assumes existence of one cache/*/index.sqlite
 
-sqlite3 -echo -cmd ".headers off" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 "SELECT priority, COUNT(*) AS count FROM page_touch GROUP BY priority; SELECT * FROM page_touch ORDER BY priority DESC, touch_time DESC LIMIT 25"
+sqlite3 -echo -cmd ".headers on" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 "SELECT priority, COUNT(*) AS count FROM page_touch GROUP BY priority; SELECT * FROM page_touch ORDER BY priority DESC, touch_time DESC LIMIT 25"
 
