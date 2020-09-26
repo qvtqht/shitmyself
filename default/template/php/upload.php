06d23eb6f3c9579ea5f6d608e72a607573f29a6a
toshiba <@>
2020-09-26 10:21:35 -0400

    Fix redirection in upload.pl
    Clean up and improve debug output

--- default/template/php/upload.php
+++ default/template/php/upload.php
@@ -5,109 +5,72 @@ include_once('utils.php');
 if (!empty($_FILES['uploaded_file'])) {
 	$basePath = "image/";
 	WriteLog('$basePath = ' . $basePath);
+	WriteLog('$_FILES: ' . print_r($_FILES, 1));
 
-	WriteLog(print_r($_FILES, 1));
+	if ($_FILES['uploaded_file']['error']) {
+		// See https://www.php.net/manual/en/features.file-upload.errors.php
+		echo "There was an error uploading the file.";
 
-//
-//	if (file_exists($path)) {
-//		//		$hash = GetFileHash($path);
-//		//
-//		//		// path for new html file
-//		//		$fileHtmlPath = './' . GetHtmlFilename($hash);
-//		//
-//		//		// path for client's (browser's) path to html file
-//		//		$fileUrlPath = '/' . GetHtmlFilename($hash);
-//		//
-//		//		$redirectUrl = '';
-//		//
-//		//		if (!$redirectUrl && file_exists($fileHtmlPath) && $fileUrlPath) {
-//		//			$itemPostedServerResponse = "Success! Item already posted!";
-//		//			$itemPostedServerResponseId = StoreServerResponse($itemPostedServerResponse);
-//		//			$redirectUrl = $fileUrlPath . '?message=' . $itemPostedServerResponseId;
-//		//		}
-//		//
-//		//		if ($redirectUrl) {
-//		//			WriteLog('Location: ' . $redirectUrl);
-//		//			header('Location: ' . $redirectUrl);
-//		//		}
-//
-//		WriteLog("File with that name already exists on upload.php");
-//		//echo "File with that name already exists!";
-//	}
-//
-//
-	{
-		WriteLog('$_FILES: ' . print_r($_FILES, 1));
+		if ($_FILES['uploaded_file']['error'] == 1) {
+			echo "The problem may be related to the file's size";
+		}
+	} else {
+		$path = $basePath . basename($_FILES['uploaded_file']['name']);
+		WriteLog('trying to move_uploaded_file(' . $_FILES['uploaded_file']['tmp_name'] . ',' . $path . ')');
+		if (file_exists($path)) {
+			#todo make this nicer
+			$path = $basePath . time() . '_' . basename($_FILES['uploaded_file']['name']);
+		}
 
-		if ($_FILES['uploaded_file']['error']) {
-			// See https://www.php.net/manual/en/features.file-upload.errors.php
-			echo "There was an error uploading the file.";
+		$moveFileResult = move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path);
 
-			if ($_FILES['uploaded_file']['error'] == 1) {
-				echo "The problem may be related to the file's size";
-			}
+		if (!$moveFileResult) {
+			WriteLog("There was an error uploading the file, please try again! move_uploaded_file() returned: [$moveFileResult]");
+			echo "There was a problem uploading the file, please try again!";
 		} else {
-			$path = $basePath . basename($_FILES['uploaded_file']['name']);
+			# remember current working directory, we'll need it later
+			$pwd = getcwd();
+			WriteLog('$pwd = ' . $pwd);
 
-			WriteLog('trying to move_uploaded_file(' . $_FILES['uploaded_file']['tmp_name'] . ',' . $path . ')');
-
-			if (file_exists($path)) {
-				// #todo make this nicer
-				$path = $basePath . time() . '_' . basename($_FILES['uploaded_file']['name']);
+			if (GetConfig('admin/php/post/update_all_on_post')) {
+				WriteLog("cd .. ; ./update.pl --all");
+				WriteLog(`cd .. ; ./update.pl --all`);
+			}
+			elseif (GetConfig('admin/php/post/update_on_post')) {
+				WriteLog("cd .. ; ./update.pl");
+				WriteLog(`cd .. ; ./update.pl`);
+			}
+			elseif (GetConfig('admin/php/post/update_item_on_post')) {
+				WriteLog("cd .. ; ./update.pl \"html/$path\"");
+				WriteLog(`cd .. ; ./update.pl "html/$path"`);
 			}
 
-			$moveFileResult = move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path);
+			if ($pwd) {
+				WriteLog("cd $pwd");
+				WriteLog(`cd $pwd`);
+			}
 
-			if (!$moveFileResult) {
-				WriteLog("There was an error uploading the file, please try again! move_uploaded_file() returned: [$moveFileResult]");
-				echo "There was a problem uploading the file, please try again!";
+			$hash = GetFileHash($path); // get file's hash
+			$fileHtmlPath = './' . GetHtmlFilename($hash); // path for new html file
+			$fileUrlPath = '/' . GetHtmlFilename($hash); // path for client's (browser's) path to html file
+
+			WriteLog('upload.pl: $hash = ' . $hash);
+			WriteLog('upload.pl: $fileHtmlPath = ' . $fileHtmlPath);
+			WriteLog('upload.pl: $fileUrlPath = ' . $fileUrlPath);
+			WriteLog('upload.pl: file_exists($fileHtmlPath) = ' . file_exists($fileHtmlPath));
+			
+			if (file_exists($fileHtmlPath) && $fileUrlPath) {
+				RedirectWithResponse($fileUrlPath, 'Success! Thank you for uploading this beautiful picture!');
 			} else {
-				// remember current working directory, we'll need it later
-				$pwd = getcwd();
-				WriteLog('$pwd = ' . $pwd);
-
-				if (GetConfig('admin/php/post/update_all_on_post')) {
-					WriteLog("cd .. ; ./update.pl --all");
-					WriteLog(`cd .. ; ./update.pl --all`);
-				}
-				elseif (GetConfig('admin/php/post/update_on_post')) {
-					WriteLog("cd .. ; ./update.pl");
-					WriteLog(`cd .. ; ./update.pl`);
-				}
-				elseif (GetConfig('admin/php/post/update_item_on_post')) {
-					WriteLog("cd .. ; ./update.pl \"html/$path\"");
-					WriteLog(`cd .. ; ./update.pl "html/$path"`);
-				}
-
-				if ($pwd) {
-					WriteLog("cd $pwd");
-					WriteLog(`cd $pwd`);
-				}
-
-				$hash = GetFileHash($path);
-
-				// path for new html file
-				$fileHtmlPath = './' . GetHtmlFilename($hash);
-
-				// path for client's (browser's) path to html file
-				$fileUrlPath = '/' . GetHtmlFilename($hash);
-
-				$redirectUrl = '';
-
-				if (!$redirectUrl && file_exists($fileHtmlPath) && $fileUrlPath) {
-					RedirectWithResponse($fileUrlPath, 'Success! Thank you for uploading this beautiful pictures!');
-				} else {
-					RedirectWithResponse('/post.html', 'Image uploaded, and should appear shortly after it is processed.');
-				}
+				// good enough for now, eventually would be nice if it went to the actual item #todo
+				RedirectWithResponse('/index0.html', 'Image uploaded, and should appear shortly after it is processed.');
 			}
 		}
 	}
 
-	print WriteLog('');
-//#todo
-// if (GetConfig('admin/php/debug')) {
-//	$html = str_replace('</body>', WriteLog('') . '</body>', $html);
-// }
-// print($html);
+	print WriteLog(''); // dump log to page if in debug mode
 
-}
+	if (GetConfig('admin/php/debug')) {
+		$html = str_replace('</body>', WriteLog('') . '</body>', $html);
+	}
+} # if (!empty($_FILES['uploaded_file']))
