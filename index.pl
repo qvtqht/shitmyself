3a918dbf51b790d2746ec9d84159863da5467e76
toshiba <@>
2020-10-02 00:28:02 -0400

    Add progress output for chain import

--- index.pl
+++ index.pl
@@ -31,6 +31,9 @@ sub MakeChainIndex { # reads from log/chain.log and puts it into item_attribute 
 			my $sequenceNumber = 0;
 
 			foreach my $currentLine (@addedRecord) {
+				WriteLog("MakeChainIndex: $currentLine");
+				WriteMessage("$sequenceNumber");
+
 				my ($fileHash, $addedTime, $proofHash) = split('\|', $currentLine);
 
 				my $expectedHash = md5_hex($previousLine . '|' . $fileHash . '|' . $addedTime);
