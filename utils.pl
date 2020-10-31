f55600380186117967c264677a09ffd09ad8101e
toshiba <@>
2020-10-31 16:46:43 -0400

    Cleanup

--- utils.pl
+++ utils.pl
@@ -2176,7 +2176,9 @@ sub WriteMessage { # Writes timestamped message to console (stdout)
 		state @chars;
 		if (!@chars) {
 			#@chars = qw(, . - ' `); # may generate warning
-			@chars = (',', '.', '-', "'", '`'); # may generate warning
+			@chars = (',', '.', '-', "'", '`');
+			#@chars = ('.', ',');
+			#@chars = (qw(0 1 2 3 4 5 6 7 8 9 A B C D E F));
 		}
 
 		#my @chars=('a'..'f','0'..'9');
