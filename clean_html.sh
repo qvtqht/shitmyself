f329d75bc911e250df283da96e4d8733173f2233
toshiba <@>
2020-11-07 18:13:14 -0500

    Add clean_html.sh

---
+++ clean_html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this script will clean html to make room for new html
+
+# mkdir trash
+# mkdir trash.`date +%s`
+# #todo first move to trash, then rm. reason: rm takes longer than mv
+
+echo "rm -rf html/*.html html/*/*.html html/*/*/*.html"
+rm -rf html/*.html html/*/*.html html/*/*/*.html
+
+echo "rm -rf html/*.js html/*/*.js html/*/*/*.js"
+rm -rf html/*.js html/*/*.js html/*/*/*.js
