a4bd94b213bd3a098ef71eb74ed1d905a3953458
toshiba <@>
2020-11-27 21:13:57 -0500

    route.php: add comments in TrimPath()

--- default/template/php/route.php
+++ default/template/php/route.php
@@ -27,6 +27,7 @@ function SetHtmlClock ($html) { // sets html clock on page if present
 }
 
 function TrimPath ($string) { // Trims the directories AND THE FILE EXTENSION from a file path
+# Should really be called GetFileNameWithoutPathAndExtension()
 	while (index($string, "/") >= 0) {
 		$string = substr($string, index($string, "/") + 1);
 	}
@@ -36,7 +37,7 @@ function TrimPath ($string) { // Trims the directories AND THE FILE EXTENSION fr
 	}
 
 	return $string;
-}
+} # TrimPath()
 
 function TranslateEmoji ($html) { // replaces emoji with respective text
 	WriteLog('TranslateEmoji()');
