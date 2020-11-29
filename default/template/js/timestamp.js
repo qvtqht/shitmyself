3be02aad5923388e84009916ad10344c20526aa8
toshiba <@>
2020-11-29 07:05:48 -0500

    Add comments in ShowTimestamps() and timestamp.js

--- default/template/js/timestamp.js
+++ default/template/js/timestamp.js
@@ -85,46 +85,51 @@ function LongAgo(seconds) { // returns string with time units
 }
 
 function ShowTimestamps () { // finds any class=timestamp, updates its displayed time as needed
+// currently requires getElementsByClassName()
+// in the future, ie4+, nn4+, and others compat can be improved
+
 	//alert('DEBUG: ShowTimestamps()');
 	if (document.getElementsByClassName) {
 		//alert('DEBUG: ShowTimestamps: document.getElementsByClassName feature check passed');
-
 		var d = new Date();
 		var curTime = Math.floor(d.getTime() / 1000);
 		var changeLogged = 0;
 	
 		// find elements with class=timestamp
-		var te = document.getElementsByClassName("timestamp"); // #todo nn3 compat for loop
+		var te = document.getElementsByClassName("timestamp");
 
 		//alert('DEBUG: ShowTimestamps: class=timestamp elements found: ' + te.length);
-
 		for (var i = 0; i < te.length; i++) {
+			// loop through all the timestamp elements on the page
 			if (!isNaN(te[i].getAttribute('epoch'))) {
-				// element also has an attribute called 'epoch', and it is a number
-				var secs = 0 - (curTime - te[i].getAttribute('epoch')); // number of seconds since epoch number
-				var longAgo = LongAgo(secs);
+				// element also has an attribute called 'epoch', and it is
+				// a number, which would represent epoch seconds
+				var secs = 0 - (curTime - te[i].getAttribute('epoch')); // number of seconds since epoch begin
+				var longAgo = LongAgo(secs); // what the element's displayed value should be
 				if (te[i].innerHTML != longAgo) {
 					// element's content does not already equal what it should equal
 					te[i].innerHTML = longAgo;
 					if ((secs * (-1)) < 3600) {
+						// less than an hour ago = bold
 						te[i].style.fontWeight = 'bold';
 					} else {
 						te[i].style.fontWeight = '';
 					}
 					if ((secs * (-1)) < 86400) {
+						// less than a day ago = highlight
 						te[i].style.backgroundColor = '$colorHighlightAlert';
 					} else {
 						te[i].style.backgroundColor = '';
 					}
-					changeLogged++;
+					changeLogged++; // count change logged
 				}
 			}
 		}
 
 		if (window.EventLoop) {
-			// do nothing
+			// do nothing, EventLoop() will call us when needed
 		} else {
-			// allows ShowTimestamps() to run decoupled from main EventLoop
+			// allow ShowTimestamps() to run decoupled from EventLoop()
 			if (changeLogged) {
 				setTimeout('ShowTimestamps()', 5000);
 			} else {
@@ -135,10 +140,10 @@ function ShowTimestamps () { // finds any class=timestamp, updates its displayed
 } // ShowTimestamps()
 
 if (window.EventLoop) {
-	// do nothing
+	// do nothing, EventLoop() will take care of us
 } else {
+	// if no EventLoop(), we do it ourselves
 	ShowTimestamps();
 }
- // #todo this should probably be called from onload somehow
 
 // == end timestamp.js
