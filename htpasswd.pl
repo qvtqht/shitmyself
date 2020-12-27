ccefa05bb9c6b83aff5fd892151988d886727842
qwerty <qwerty@>
2020-12-27 02:58:14 -0500

    htpasswd.pl : turn it into a module

--- htpasswd.pl
+++ htpasswd.pl
@@ -1,6 +1,31 @@
 #!/usr/bin/perl
 use strict;
+
+#require './utils.pl';
+
+sub GetHtpasswdLine {
+	my $username = shift;
+	my $password = shift;
+
+	#todo sanity checks
+
+	if ($username && $password) {
+		my $passwordEncrypted = crypt($password,$username);
+		my $line = $username . ":" . $passwordEncrypted;
+
+		return $line;
+	} else {
+		return '';
+	}
+} # GetHtpasswdLine()
+
 chomp(my $username=$ARGV[0]);
 chomp(my $password=$ARGV[1]);
-print $username.":".crypt($password,$username)."\n";
 
+if ($username && $password) {
+	my $line = GetHtpasswdLine($username, $password);
+	print $line . "\n";
+}
+
+
+1;
