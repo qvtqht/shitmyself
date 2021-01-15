dc865f8680e57d4088c801ffa830f4bdcf1312f2
toshiba <@>
2021-01-14 23:35:33 -0500

    improve output of validate.pl

--- validate.pl
+++ validate.pl
@@ -13,10 +13,18 @@ require './index.pl';
 
 if (GetConfig('admin/read_chain_log')) {
 	WriteMessage('admin/read_chain_log is true, performing chain validation...');
-	my $chainResult = MakeChainIndex(0);
-	if ($chainResult) {
+	my %chainResult = MakeChainIndex(0);
+	if (%chainResult) {
 		WriteMessage('Chain validation success!');
+
+		WriteMessage('Chain Sequence Length: ' . $chainResult{'chain_sequence'});
+		WriteMessage('Next Item Stub: ' . $chainResult{'chain_next'});
+		WriteMessage('Timestamp: ' . $chainResult{'chain_timestamp'});
+#
+#		$return{'chain_sequence'} = $sequenceNumber;
+#		$return{'chain_next'} = $previousLine;
+#		$return{'chain_timestamp'} = $addedTime;
 	} else {
-		WriteMessage('Chain validation FAILURE!');
+		WriteMessage('WARNING! Chain validation FAILURE!');
 	}
 }
