2af0399175be7dc7e80f373aa83fbabf63e5376a
toshiba <@>
2021-02-04 23:41:34 -0500

    rebuild_dev: catch stderr output from scripts

--- rebuild_dev.sh
+++ rebuild_dev.sh
@@ -6,9 +6,9 @@
 # rebuilds entire site
 # calls update.pl --all to re-import all data
 
-./clean_dev.sh
+./clean_dev.sh 2>>log/clean_dev_err.txt
 
-./build.pl
+./build.pl 2>>log/build_err.txt
 
-./index.pl --all
+./index.pl --all 2>>log/index_err.txt
 
