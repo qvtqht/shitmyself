eca63f26caffb0d9f5d306a816803edce8fe6443
toshiba <@>
2021-02-09 17:17:41 -0500

    loading_begin.js : fix bug in addLoadingIndicator(), add a bunch of debug output, replace duplicate code with function call

--- default/template/js/loading_begin.js
+++ default/template/js/loading_begin.js
@@ -4,27 +4,41 @@
 //var loadingIndicatorWaitToHideMin = 500;
 
 function addLoadingIndicator (strMessage) { // adds loading indicator bar (to top of page, depending on style)
-	if (!strMessage) {
+	//alert('DEBUG: addLoadingIndicator(' + strMessage + ')');
+	if (strMessage == undefined || !strMessage) {
+		//alert('DEBUG: strMessage = ' + strMessage);
 		strMessage = 'Loading...';
+	}
+	//alert('DEBUG: addLoadingIndicator: strMessage = ' + strMessage);
+
+	if (!document.createElement) {
+		//alert('DEBUG: addLoadingIndicator: warning: no document.createElement');
+		return '';
+		// #todo improve compatibility here
 	}
 
 	var spanLoadingIndicator = document.createElement('span');
-	spanLoadingIndicator.setAttribute('id', 'spanLoadingIndicator');
+	//alert('DEBUG: addLoadingIndicator 1');
+	spanLoadingIndicator.setAttribute('id', 'loadingIndicator');
+	//alert('DEBUG: addLoadingIndicator 2');
 	spanLoadingIndicator.innerHTML = strMessage;
+	//alert('DEBUG: addLoadingIndicator 3');
+	spanLoadingIndicator.zIndex = 1337;
+	//alert('DEBUG: addLoadingIndicator 4');
 	document.body.appendChild(spanLoadingIndicator);
+	//alert('DEBUG: addLoadingIndicator 5');
+
 } // addLoadingIndicator()
 
 if (document.createElement) {
+	//alert('DEBUG: loading_begin.js: createElement feature check PASSED!');
 	var d = new Date();
 	var loadingIndicatorStart = d.getTime() * 1;
 	var gt = unescape('%3E');
 
-//	var loadingIndicatorloadCounter = 0;
-	var loadingIndicator = document.createElement('span');
-	if (loadingIndicator) {
-		loadingIndicator.setAttribute('id', 'loadingIndicator');
-		loadingIndicator.innerHTML = 'Loading...';
-		document.body.appendChild(loadingIndicator);
-	}
+	//var loadingIndicatorloadCounter = 0;
+	addLoadingIndicator('Loading...');
+} else {
+	//alert('DEBUG: loading_begin.js: createElement feature check FAILED!');
 }
 // end loading_begin.js
