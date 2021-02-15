5eaf90407dd393e29d4550af8c48c97405c58715
toshiba <@>
2021-02-15 14:10:36 -0500

    dragging.js: clean up and add comments and debug output ; add basic zIndex management ;

--- default/template/js/dragging.js
+++ default/template/js/dragging.js
@@ -1,4 +1,16 @@
 /* dragging.js */
+// allows dragging of boxes on page with the mouse pointer
+
+/*
+	known issues:
+	* problem: syntax errors on older browsers like netscape
+	  solution: remove nested function declarations
+	* problem: no keyboard alternative at this time
+	  solution: somehow allow moving through windows and moving them with keyboard
+	* problem: slow and janky, needs more polish
+	  solution: optimizations
+*/
+
 // props https://www.w3schools.com/howto/howto_js_draggable.asp
 
 /*
@@ -12,9 +24,13 @@
     	}
 */
 
-function dragElement (elmnt, header) {
-	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+window.draggingZ = 0; // keeps track of the topmost box's zindex
+// incremented whenever dragging is initiated, that way element pops to top
 
+function dragElement (elmnt, header) {
+	//alert('DEBUG: dragElement');
+
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 	if (header) {
 		// if present, the header is where you move the DIV from:
 		header.onmousedown = dragMouseDown;
@@ -23,8 +39,8 @@ function dragElement (elmnt, header) {
 		elmnt.onmousedown = dragMouseDown;
 	}
 
+	// set element's position based on its initial box model position
 	var rect = elmnt.getBoundingClientRect();
-
 	elmnt.style.position = 'absolute';
 	elmnt.style.top = (rect.top) + "px";
 	elmnt.style.left = (rect.left) + "px";
@@ -34,6 +50,9 @@ function dragElement (elmnt, header) {
 	//elmnt.style.z-index = '9';
 
 	function dragMouseDown(e) {
+		//alert('DEBUG: dragMouseDown');
+		elmnt.style.zIndex = ++window.draggingZ;
+
 		e = e || window.event;
 		e.preventDefault();
 		// get the mouse cursor position at startup:
@@ -43,11 +62,10 @@ function dragElement (elmnt, header) {
 		document.onmouseup = closeDragElement;
 		// call a function whenever the cursor moves:
 		document.onmousemove = elementDrag;
-
-		elmnt.style.zIndex++;
 	}
 
 	function elementDrag(e) {
+		//alert('DEBUG: elementDrag');
 		//document.title = pos1 + ',' + pos2 + ',' + pos3 + ',' + pos4;
 		//document.title = e.clientX + ',' + e.clientY;
 		//document.title = elmnt.offsetTop + ',' + elmnt.offsetLeft;
@@ -65,6 +83,8 @@ function dragElement (elmnt, header) {
 	}
 
 	function closeDragElement() {
+		//alert('DEBUG: closeDragElement');
+
 		// stop moving when mouse button is released:
 		document.onmouseup = null;
 		document.onmousemove = null;
@@ -81,28 +101,24 @@ function dragElement (elmnt, header) {
 					if (elId && elId.innerHTML.length < 31) {
 						SetPrefs(elId.innerHTML + '.style.top', elmnt.style.top);
 						SetPrefs(elId.innerHTML + '.style.left', elmnt.style.left);
-		//				elements[i].style.top = GetPrefs(elId.innerHTML + '.style.top') || elId.style.top;
-		//				elements[i].style.left = GetPrefs(elId.innerHTML + '.style.left') || elId.style.left;
+						//elements[i].style.top = GetPrefs(elId.innerHTML + '.style.top') || elId.style.top;
+						//elements[i].style.left = GetPrefs(elId.innerHTML + '.style.left') || elId.style.left;
 					} else {
 						//alert('DEBUG: DraggingInit: elId is false');
 					}
 				}
 			}
-			elmnt.style.zIndex++;
 		}
-//
-//		if (elmnt.id) {
-//			if (window.SetPrefs) {
-//				SetPrefs(elmnt.id + '.style.top', elmnt.style.top);
-//				SetPrefs(elmnt.id + '.style.left', elmnt.style.left);
-//			}
-//		}
 	}
 }
 
 function DraggingInit (doPosition) {
-// initializes all class=dialog elements on the page to be draggable
+// initialize all class=dialog elements on the page to be draggable
+
+	//alert('DEBUG: DraggingInit');
+
 	if (!document.getElementsByClassName) {
+		// feature check
 		return '';
 	}
 
@@ -118,8 +134,12 @@ function DraggingInit (doPosition) {
 	}
 
 	var elements = document.getElementsByClassName('dialog');
-//	for (var i = 0; i < elements.length; i++) {
-	for (var i = elements.length - 1; 0 <= i; i--) {
+	// for (var i = 0; i < elements.length; i++) {
+	for (var i = elements.length - 1; 0 <= i; i--) { // walk backwards for positioning reasons
+		// walking backwards is necessary to preserve the element positioning on the page
+		// once we remove the element from the page flow, all the other elements reflow to account it
+		// if we walk forwards here, all the elements will end up in the top left corner
+
 		var allTitlebar = elements[i].getElementsByClassName('titlebar');
 		var firstTitlebar = allTitlebar[0];
 
@@ -134,31 +154,6 @@ function DraggingInit (doPosition) {
 				//alert('DEBUG: DraggingInit: elId is false');
 			}
 		}
-
-
-//		if (elements[i].id && window.GetPrefs) {
-//			var elTop = GetPrefs(elements[i].id + '.style.top');
-//			var elLeft = GetPrefs(elements[i].id + '.style.left');
-//
-//			if (elTop && elLeft) {
-//				elmnt.style.left = elLeft;
-//				elmnt.style.top = elTop;
-//			}
-//
-//			//var elTop = window.elementPosCounter || 1;
-//			//var elTop = GetPrefs(elements[i].id + '.style.top');
-//			//window.elementPosCounter += elmnt.style.height;
-//
-//			//var elLeft = GetPrefs(elements[i].id + '.style.left') || 1;
-//
-//			//if (elTop && elLeft) {
-//				//elmnt.style.left = elLeft;
-//				//elmnt.style.top = elTop;
-//			//}
-//		} else {
-//			//alert('DEBUG: dragging.js: warning: id and/or GetPrefs() missing');
-//		}
-//		//dragElement(elements[i], firstTitlebar);
 	}
 } // DraggingInit()
 
